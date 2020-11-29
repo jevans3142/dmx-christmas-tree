d9818641719304a972eeed4bfe37f6f96ff94ea8
John Evans <djjevans+github@googlemail.com>
2020-11-29 21:57:47 +0000

    Working test code
    This works with the LEDs and displays an RGB pattern - no motion or DMX interaction yet

--- src/main/main.c
+++ src/main/main.c
@@ -12,12 +12,64 @@
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "freertos/queue.h" 
+#include "driver/spi_master.h"
 
 #include "main.h"
 
 
 void app_main(void)
 {
-   
-}
+    esp_err_t ret;
+    spi_bus_config_t buscfg={
+        .miso_io_num=-1,
+        .mosi_io_num=PIN_NUM_MOSI,
+        .sclk_io_num=PIN_NUM_CLK,
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1,
+        .max_transfer_sz=4096
+    };
+    spi_device_handle_t spi;
 
+    spi_device_interface_config_t devcfg={
+        .clock_speed_hz=10000,           //Clock out at 10 kHz
+        .mode=0,                                //SPI mode 0
+        .spics_io_num=-1,               //CS pin not used
+        .queue_size=7,                 
+    };
+
+    ret=spi_bus_initialize(VSPI_HOST, &buscfg, 2);
+    ret=spi_bus_add_device(VSPI_HOST, &devcfg, &spi);
+
+    uint8_t data[] = {0,0,0,0,
+        254,100,0,0,
+        254,0,100,0,
+        254,0,0,100,
+        254,100,0,0,
+        254,0,100,0,
+        254,0,0,100,        
+        254,100,0,0,
+        254,0,100,0,
+        254,0,0,100,
+           254,100,0,0,
+        254,0,100,0,
+        254,0,0,100,
+               254,100,0,0,
+        254,0,100,0,
+        254,0,0,100,
+               254,100,0,0,
+        254,0,100,0,
+        254,0,0,100,
+               254,100,0,0,
+        254,0,100,0,
+        254,0,0,100,
+               254,100,0,0,
+        254,0,100,0,
+        0,0,0,0,0};
+
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));  
+    t.length=109*8; 
+    t.tx_buffer=data;               
+
+    ret = spi_device_transmit(spi, &t);
+}
--- src/main/main.h
+++ src/main/main.h
@@ -2,4 +2,9 @@
 //--------------------
 
 //Task setup
+#define DMX_INPUT_TASK_STACK_SIZE    2048
+#define DMX_INPUT_TASK_PRIO          10
 
+//SPI
+#  define PIN_NUM_MOSI 23
+#  define PIN_NUM_CLK  18
